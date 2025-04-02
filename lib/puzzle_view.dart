4812d66bcb3a6723181003e7b41f3371202f0eec
Dansot4891 <cnctlim94@gmail.com>
2025-04-03 01:00:43 +0900

    [feat, fix] 퍼즐 게임 1차 구현 1. 이미지를 퍼즐 조각처럼 구현 	- 상단에는 조각이 빠진 틀 	- 하단은 퍼즐 조각이 랜덤으로 배열 		- 퍼즐 조각은 내부 함수화 2. 이미지 객체 구현 	- 파일 데이터, 정답여부, 인덱스 3. Drawable, dragtarget을 이용한 드래그하여 퍼즐 조각 맞추는 로직 구현 ⚠퍼즐을 맞추는데 퍼즐틀에 가져다 대기만해도 알아서 정답으로 인정되는 오류 발생 ⚠현재 퍼즐틀과 퍼즐이 범위를 침범하는 오류 발생

--- lib/puzzle_view.dart
+++ lib/puzzle_view.dart
@@ -1,7 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
+
 import 'package:jogak_jogak/main.dart';
 
 class PuzzleView extends StatefulWidget {
@@ -12,70 +16,153 @@ class PuzzleView extends StatefulWidget {
 }
 
 class _PuzzleViewState extends State<PuzzleView> {
+  // 파일
   File? file;
-  List<File> croppedFiles = [];
 
-  int gridViewSize = 6;
+  // 잘린 이미지들의 리스트
+  List<ImagePuzzle> pieces = [];
+
+  // gridview의 n x n 사이즈
+  int gridViewSize = 4;
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextButton(
-              onPressed: () async {
-                final imgFile = await ImgPicker.pickImage();
-                file = imgFile;
-                if (imgFile == null) {
-                  return;
-                }
-
-                final fileList = await ImgCrop.cropImage(
-                  imgFile,
-                  gridViewSize: gridViewSize,
-                );
-                if (fileList == null) {
-                  return;
-                }
-                croppedFiles = fileList;
-
-                setState(() {});
-              },
-              child: Text('이미지 선택'),
-            ),
-            if (croppedFiles.isNotEmpty)
-              Flexible(
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  itemCount: gridViewSize * gridViewSize,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: gridViewSize,
-                    mainAxisSpacing: 4,
-                    crossAxisSpacing: 4,
-                  ),
-                  itemBuilder: (context, index) {
-                    return Image.file(
-                      croppedFiles[index],
-                      width: width / gridViewSize,
-                      height: width / gridViewSize,
-                      fit: BoxFit.cover,
-                      cacheHeight: (width.cacheSize(context) / gridViewSize).toInt(),
-                      cacheWidth: (width.cacheSize(context) / gridViewSize).toInt(),
-                    );
-                  },
-                ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextButton(
+                onPressed: () async {
+                  final imgFile = await ImgPicker.pickImage();
+                  file = imgFile;
+                  if (imgFile == null) {
+                    return;
+                  }
+          
+                  final fileList = await ImgCrop.cropImage(
+                    imgFile,
+                    gridViewSize: gridViewSize,
+                    width: width
+                  );
+                  if (fileList == null) {
+                    return;
+                  }
+                  pieces = fileList;
+          
+                  setState(() {});
+                },
+                child: Text('이미지 선택'),
               ),
-          ],
+              if (pieces.isNotEmpty)
+                Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: GridView.builder(
+                          shrinkWrap: true,
+                          itemCount: gridViewSize * gridViewSize,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: gridViewSize,
+                          ),
+                          itemBuilder: (context, index) {
+                            return DragTarget<int>(
+                              builder: (context, candidateData, rejectedData) {
+                                final piece = pieces[index];
+                                return imagePuzzlePiece(
+                                  file: piece.file,
+                                  width: width,
+                                  isFrame: !piece.isRight,
+                                );
+                              },
+                              onAcceptWithDetails: (DragTargetDetails<int> details){
+                                setState(() {
+                                  pieces = pieces.map((e) => e.index == details.data ? e.copyWith(isRight: !e.isRight) : e).toList();
+                                });
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: Stack(
+                          children:
+                              pieces.map((e) {
+                                return Positioned(
+                                  top: e.topRandom,
+                                  left: e.leftRandom,
+                                  child: Draggable(
+                                    data: e.index,
+                                    feedback: imagePuzzlePiece(
+                                      file: e.file,
+                                      width: width,
+                                    ),
+                                    childWhenDragging: emptyBox(),
+                                    child: imagePuzzlePiece(
+                                      file: e.file,
+                                      width: width,
+                                      isRight: e.isRight,
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  SizedBox emptyBox(){
+    return SizedBox();
+  }
+
+  // 이미지 퍼즐 조각 위젯
+  // file과 너비를 변수로 받음
+  Widget imagePuzzlePiece({
+    required File file,
+    required double width,
+    bool isFrame = false,
+    bool isRight = false,
+    }) {
+      // 퍼즐틀일 경우 테두리만 가진 박스 반환
+    if(isFrame){
+      return Container(
+        width: (width / gridViewSize),
+        height: (width / gridViewSize),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey,
+          )
+        ),
+      );
+    }
+    // 하단 퍼즐 조각에서
+    // 정답이라면 SizedBox 반환
+    if(isRight){
+      return emptyBox();
+    }
+    return Image.file(
+      file,
+      width: width / gridViewSize,
+      height: width / gridViewSize,
+      fit: BoxFit.cover,
+      cacheHeight: (width.cacheSize(context) / gridViewSize).toInt(),
+      cacheWidth: (width.cacheSize(context) / gridViewSize).toInt(),
     );
   }
 }
 
 class ImgPicker {
+  // 이미지를 사용자 핸드폰으로부터 가져오기
   static Future<File?> pickImage() async {
     final picker = ImagePicker();
     final resp = await picker.pickImage(
@@ -91,34 +178,14 @@ class ImgPicker {
 }
 
 class ImgCrop {
-  // 이미지 자르기 후 저장
-  static Future<File> saveCroppedImage(
-    File file,
-    img.Image croppedImage,
-    String extension,
-  ) async {
-    final outputPath =
-        '${file.path}_crop_${DateTime.now().millisecondsSinceEpoch}.$extension';
-    final outputFile = File(outputPath);
-
-    switch (extension) {
-      case 'jpg':
-        await outputFile.writeAsBytes(img.encodeJpg(croppedImage));
-        break;
-      case 'png':
-        await outputFile.writeAsBytes(img.encodePng(croppedImage));
-        break;
-      default:
-        throw Exception('지원되지 않는 확장자');
-    }
-
-    return outputFile;
-  }
-
-  static Future<List<File>?> cropImage(File file, {
+  // image를 자르는 함수
+  static Future<List<ImagePuzzle>?> cropImage(
+    File file, {
+    // 그리드뷰 사이즈 n x n
     required int gridViewSize,
+    required double width,
   }) async {
-    final List<File> fileList = [];
+    final List<ImagePuzzle> fileList = [];
     // 이미지의 width, height를 가져오기 위해 파일 읽기
     final bytes = await file.readAsBytes();
     final image = img.decodeImage(bytes);
@@ -134,6 +201,7 @@ class ImgCrop {
 
     // 더 작은 길이를 하나의 길이로 설정
     final pieceSize = pieceWidth >= pieceHeight ? pieceHeight : pieceWidth;
+    int index = 0;
 
     // 자른 이미지들을 리스트에 추가
     for (int y = 0; y < gridViewSize; y++) {
@@ -163,17 +231,60 @@ class ImgCrop {
           default:
             throw Exception('지원되지 않는 확장자');
         }
-        fileList.add(outputFile);
+        final topRandom =
+                                    10 + Random().nextDouble() * width / 3 * 2;
+                                final leftRandom =
+                                    10 + Random().nextDouble() * width / 3 * 2;
+        final imgPuzzle = ImagePuzzle(
+          index: index,
+          file: outputFile,
+          topRandom: topRandom,
+          leftRandom: leftRandom,
+        );
+        index++;
+        fileList.add(imgPuzzle);
       }
     }
     return fileList;
   }
 }
 
-// ---- extension ----
+// ---- image cache size extension ----
 extension CacheImageExtension on num {
   int cacheSize(BuildContext context) {
     final devicePixel = MediaQuery.of(context).devicePixelRatio;
     return (this * devicePixel).toInt();
+  }
+}
+
+class ImagePuzzle {
+  final int index;
+  final File file;
+  final bool isRight;
+  final double topRandom;
+  final double leftRandom;
+
+  const ImagePuzzle({
+    required this.index,
+    required this.file,
+    required this.topRandom,
+    required this.leftRandom,
+    this.isRight = false,
+  });
+
+  ImagePuzzle copyWith({
+    int? index,
+    File? file,
+    bool? isRight,
+    double? topRandom,
+    double? leftRandom,
+  }) {
+    return ImagePuzzle(
+      index: index ?? this.index,
+      file: file ?? this.file,
+      isRight: isRight ?? this.isRight,
+      topRandom: topRandom ?? this.topRandom,
+      leftRandom: leftRandom ?? this.leftRandom,
+    );
   }
 }
