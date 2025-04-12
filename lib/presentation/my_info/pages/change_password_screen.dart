833138c4039e57ea2114125651d6186b7ed08041
Dansot4891 <cnctlim94@gmail.com>
2025-04-13 00:01:01 +0900

    [feat] 비밀번호/닉네임 변경 ui 추가 및 라우팅 연동

--- lib/presentation/my_info/pages/change_password_screen.dart
+++ lib/presentation/my_info/pages/change_password_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jogak_jogak/presentation/base/pages/base_page.dart';
 import 'package:jogak_jogak/presentation/base/widgets/appbar/default_appbar.dart';
 import 'package:jogak_jogak/presentation/base/widgets/button/app_button.dart';
+import 'package:jogak_jogak/presentation/base/widgets/text_field/text_field.dart';
 
 class ChangePasswordScreen extends StatelessWidget {
   const ChangePasswordScreen({super.key});
@@ -9,8 +10,30 @@ class ChangePasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BasePage(
-      appBar: const DefaultAppbar(title: '닉네임 변경'),
-      body: Column(children: [AppButton(text: '닉네임 변경', onTap: () {})]),
+      appBar: const DefaultAppbar(title: '비밀번호 변경'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            CustomTextFormField(
+              controller: TextEditingController(),
+              hintText: '현재 비밀번호',
+            ),
+            const SizedBox(height: 32),
+            CustomTextFormField(
+              controller: TextEditingController(),
+              hintText: '새 비밀번호',
+            ),
+            const SizedBox(height: 8),
+            CustomTextFormField(
+              controller: TextEditingController(),
+              hintText: '새 비밀번호 확인',
+            ),
+            const SizedBox(height: 16),
+            AppButton(text: '닉네임 변경', onTap: () {}),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/presentation/my_info/pages/update_nickname_screen.dart
+++ lib/presentation/my_info/pages/update_nickname_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jogak_jogak/presentation/base/pages/base_page.dart';
 import 'package:jogak_jogak/presentation/base/widgets/appbar/default_appbar.dart';
 import 'package:jogak_jogak/presentation/base/widgets/button/app_button.dart';
+import 'package:jogak_jogak/presentation/base/widgets/text_field/text_field.dart';
 
 class UpdateNicknameScreen extends StatelessWidget {
   const UpdateNicknameScreen({super.key});
@@ -9,8 +10,28 @@ class UpdateNicknameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BasePage(
-      appBar: const DefaultAppbar(title: '비밀번호 변경'),
-      body: Column(children: [AppButton(text: '닉네임 변경', onTap: () {})]),
+      appBar: const DefaultAppbar(title: '닉네임 변경'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: CustomTextFormField(
+                    controller: TextEditingController(),
+                    hintText: '새로운 닉네임',
+                  ),
+                ),
+                const SizedBox(width: 8),
+                AppButton(text: '중복확인', horizontalPadding: 8, onTap: () {}),
+              ],
+            ),
+            const SizedBox(height: 16),
+            AppButton(text: '닉네임 변경', onTap: () {}),
+          ],
+        ),
+      ),
     );
   }
 }
