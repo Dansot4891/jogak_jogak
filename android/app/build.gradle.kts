b7c361456651d77cda6eb31e22f42adb3deca967
Dansot4891 <cnctlim94@gmail.com>
2025-04-15 18:29:20 +0900

    [chore] 앱 패키지명 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ require(detStorePassword != null) { "storePassword not found in key.properties f
 
 
 android {
-    namespace = "com.example.jogak_jogak"
+    namespace = "com.kr.jogak_jogak"
     compileSdk = flutter.compileSdkVersion
     // NDK 버전을 명시적으로 설정
     ndkVersion = "27.0.12077973"
@@ -50,7 +50,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.jogak_jogak"
+        applicationId = "com.kr.jogak_jogak"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
