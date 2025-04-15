1d43a93745ba09074d0094f1504a9d8b56508fd0
Dansot4891 <cnctlim94@gmail.com>
2025-04-16 00:40:14 +0900

    [feat] 파이어베이스에서 퍼즐 데이터를 가져오는 Repositor 로직 구현 완료

--- lib/feature/puzzle/data/repository/puzzle_repository.dart
+++ lib/feature/puzzle/data/repository/puzzle_repository.dart
@@ -1,5 +1,6 @@
-// import 'package:jogak_jogak/core/module/error_handling/result.dart';
+import 'package:jogak_jogak/core/module/error_handling/result.dart';
+import 'package:jogak_jogak/feature/puzzle/data/model/puzzle_image.dart';
 
-// abstract interface class PuzzleRepository {
-//   Future<Result<Puzzle
-// }
+abstract interface class PuzzleRepository {
+  Future<Result<PuzzleImage>> getRandomImageUrl();
+}
---
+++ lib/feature/puzzle/data/repository/puzzle_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:jogak_jogak/core/module/error_handling/result.dart';
+import 'package:jogak_jogak/core/module/exception/custom_exception.dart';
+import 'package:jogak_jogak/feature/puzzle/data/model/puzzle_image.dart';
+import 'package:jogak_jogak/feature/puzzle/data/repository/puzzle_repository.dart';
+import 'package:jogak_jogak/feature/puzzle/domain/data_source/puzzle_data_source.dart';
+import 'package:jogak_jogak/feature/puzzle/domain/mapper/puzzle_image_mapper.dart';
+
+class PuzzleRepositoryImpl implements PuzzleRepository {
+  final PuzzleDataSource _dataSource;
+
+  PuzzleRepositoryImpl(this._dataSource);
+
+  @override
+  Future<Result<PuzzleImage>> getRandomImageUrl() async {
+    try {
+      final puzzleImageDto = await _dataSource.getRandomImageUrl();
+      final puzzleImage = puzzleImageDto.toPuzzleImage();
+      if (puzzleImage == null) {
+        return const Result.error(FailedToFetchImageException());
+      }
+      return Result.success(puzzleImage);
+    } catch (e) {
+      return const Result.error(UnexpectedException());
+    }
+  }
+}
