a7950522d9504e0a16e9c1f7c6c2cf02b89a3832
Dansot4891 <cnctlim94@gmail.com>
2025-04-18 15:47:02 +0900

    [feat] 유저 정보를 전역적으로 관리하는 userprovider 1차 구현

--- lib/presentation/user/provider/user_provider.dart
+++ lib/presentation/user/provider/user_provider.dart
@@ -1,74 +1,52 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:jogak_jogak/core/module/error_handling/result.dart';
-// import 'package:jogak_jogak/core/module/state/base_state.dart';
-// import 'package:jogak_jogak/feature/auth/domain/repository/auth_repository.dart';
-// import 'package:jogak_jogak/feature/user/domain/model/user.dart';
-// import 'package:jogak_jogak/feature/user/domain/repository/user_repository.dart';
-// import 'package:jogak_jogak/presentation/user/state/user_state.dart';
+import 'package:flutter/foundation.dart';
+import 'package:jogak_jogak/core/module/error_handling/result.dart';
+import 'package:jogak_jogak/core/module/state/base_state.dart';
+import 'package:jogak_jogak/feature/auth/domain/use_case/sign_in_use_case.dart';
+import 'package:jogak_jogak/feature/user/domain/model/user.dart';
+import 'package:jogak_jogak/feature/user/domain/repository/user_repository.dart';
+import 'package:jogak_jogak/presentation/user/state/user_state.dart';
 
-// class UserProvider extends ChangeNotifier {
-//   final AuthRepository _authRepository;
-//   final UserRepository _userRepository;
+class UserProvider extends ChangeNotifier {
+  final SignInUseCase _signInUseCase;
+  final UserRepository _userRepository;
 
-//   UserProvider({
-//     required AuthRepository authRepository,
-//     required UserRepository userRepository,
-//   }) : _authRepository = authRepository,
-//        _userRepository = userRepository;
+  UserProvider({
+    required SignInUseCase signInUseCase,
+    required UserRepository userRepository,
+  }) : _signInUseCase = signInUseCase,
+       _userRepository = userRepository;
 
-//   UserState _state = const UserState();
-//   UserState get state => _state;
+  UserState _state = const UserState();
+  UserState get state => _state;
 
-//   // 로그인
-//   void signIn({required String email, required String password}) async {
-//     final loginResult = await _authRepository.signIn(
-//       email: email,
-//       password: password,
-//     );
-//     // 로그인 결과
-//     switch (loginResult) {
-//       case Success<String>():
-//         final userResult = await _userRepository.getUser(loginResult.data);
-//         // 유저 정보 조회 결과
-//         switch (userResult) {
-//           case Success<AppUser>():
-//             _state = state.copyWith(
-//               user: userResult.data,
-//               state: BaseState.success,
-//             );
-//           case Error():
-//             _state = state.copyWith(
-//               state: BaseState.error,
-//               error: userResult.error.message,
-//             );
-//         }
-//       case Error():
-//         _state = state.copyWith(
-//           state: BaseState.error,
-//           error: loginResult.error.message,
-//         );
-//     }
-//   }
-
-//   // 회원가입
-//   void signUp({
-//     required String email,
-//     required String password,
-//     required String username,
-//   }) async {
-//     final signUpResult = await _authRepository.signUp(
-//       email: email,
-//       password: password,
-//       username: username,
-//     );
-//     switch (signUpResult) {
-//       case Success<void>():
-//         _state = state.copyWith(state: BaseState.success);
-//       case Error():
-//         _state = state.copyWith(
-//           state: BaseState.error,
-//           error: signUpResult.error.message,
-//         );
-//     }
-//   }
-// }
+  // 로그인
+  void signIn({required String email, required String password}) async {
+    final loginResult = await _signInUseCase.execute(
+      email: email,
+      password: password,
+    );
+    // 로그인 결과
+    switch (loginResult) {
+      case Success<String>():
+        final userResult = await _userRepository.getUser(loginResult.data);
+        // 유저 정보 조회 결과
+        switch (userResult) {
+          case Success<AppUser>():
+            _state = state.copyWith(
+              user: userResult.data,
+              state: BaseState.success,
+            );
+          case Error():
+            _state = state.copyWith(
+              state: BaseState.error,
+              error: userResult.error.message,
+            );
+        }
+      case Error():
+        _state = state.copyWith(
+          state: BaseState.error,
+          error: loginResult.error.message,
+        );
+    }
+  }
+}
