83e1a585c0a3d0803baeb5eb0da2569409bc4b7d
Dansot4891 <cnctlim94@gmail.com>
2025-04-18 09:45:42 +0900

    [refactor] 랭킹도 퍼즐과 동일하게 pages내에 뷰모델과 state 파일이 들어가도록 변경

--- lib/presentation/rank/pages/ranking_view_model.dart
+++ lib/presentation/rank/pages/ranking_view_model.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:jogak_jogak/core/module/error_handling/result.dart';
 import 'package:jogak_jogak/core/module/state/base_state.dart';
 import 'package:jogak_jogak/feature/ranking/domain/use_case/get_rankings_use_case.dart';
-import 'package:jogak_jogak/presentation/rank/state/ranking_state.dart';
+import 'package:jogak_jogak/presentation/rank/pages/ranking_state.dart';
 
 class RankingViewModel with ChangeNotifier {
   final GetRankingsUseCase _getRankingsUseCase;
