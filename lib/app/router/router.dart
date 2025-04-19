ac1a208c5b6d73ccbff90c42806af01724afb6ca
Dansot4891 <cnctlim94@gmail.com>
2025-04-19 10:36:19 +0900

    [feat, build] 버전 업글, 로그인에서 회원가입으로 가지지 않던 현상 수정

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -14,18 +14,18 @@ class AppRouter {
       navigatorKey: _rootNavigatorKey,
       refreshListenable: userProvider,
       redirect: (context, state) {
-        print(userProvider.state.user);
         // 유저 정보가 있는데 화면이 null이면
         // 홈 화면으로 강제 이동
         if (userProvider.state.user != null &&
             state.fullPath == AppRoute.signIn.path) {
           return AppRoute.home.path;
         }
-        // 유저 정보가 없는데 회원가입/로그인 화면이 아니라면
+        // 유저 정보가 없는데 회원가입/로그인/비밀번호 재설정 화면이 아니라면
         // 로그인 화면으로 강제 이동
         else if (userProvider.state.user == null &&
-            (state.fullPath != AppRoute.signIn.path ||
-                state.fullPath != AppRoute.signUp.path)) {
+            (state.fullPath != AppRoute.signIn.path &&
+                state.fullPath != AppRoute.signUp.path &&
+                state.fullPath != AppRoute.changePw.path)) {
           return AppRoute.signIn.path;
         }
         return null;
