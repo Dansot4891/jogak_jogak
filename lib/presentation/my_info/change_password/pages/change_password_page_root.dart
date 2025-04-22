e1f5e2713ddeaa00cb8e36a589f752604f6b9ea6
Dansot4891 <cnctlim94@gmail.com>
2025-04-22 19:03:14 +0900

    [refactor] 비밀번호 변경시 나타나는 dialog를 stream을 활용하여 구조 변경

--- lib/presentation/my_info/change_password/pages/change_password_page_root.dart
+++ lib/presentation/my_info/change_password/pages/change_password_page_root.dart
@@ -1,23 +1,57 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:jogak_jogak/core/helper/dialog_service/app_show_dialog.dart';
 import 'package:jogak_jogak/presentation/base/pages/base_page.dart';
 import 'package:jogak_jogak/presentation/base/widgets/appbar/default_appbar.dart';
+import 'package:jogak_jogak/presentation/base/widgets/dialog/app_dialog.dart';
+import 'package:jogak_jogak/presentation/my_info/change_password/pages/change_password_event.dart';
 import 'package:jogak_jogak/presentation/my_info/change_password/pages/change_password_page.dart';
 import 'package:jogak_jogak/presentation/my_info/change_password/pages/change_password_view_model.dart';
 
-class ChangePasswordPageRoot extends StatelessWidget {
+class ChangePasswordPageRoot extends StatefulWidget {
   final ChangePasswordViewModel viewModel;
   const ChangePasswordPageRoot(this.viewModel, {super.key});
+
+  @override
+  State<ChangePasswordPageRoot> createState() => _ChangePasswordPageRootState();
+}
+
+class _ChangePasswordPageRootState extends State<ChangePasswordPageRoot> {
+  late StreamSubscription? _sub;
+
+  @override
+  void initState() {
+    _sub = widget.viewModel.eventStream.listen((event) {
+      if (mounted) {
+        switch (event) {
+          case ShowCheckDialog():
+            AppShowDialog.show(
+              context,
+              AppDialog.singleBtn(title: event.message, btnText: '확인'),
+            );
+        }
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BasePage(
       appBar: const DefaultAppbar(title: '비밀번호 변경'),
       body: ListenableBuilder(
-        listenable: viewModel,
+        listenable: widget.viewModel,
         builder: (context, child) {
           return ChangePasswordPage(
-            state: viewModel.state,
-            onAction: viewModel.onAction,
+            state: widget.viewModel.state,
+            onAction: widget.viewModel.onAction,
           );
         },
       ),
