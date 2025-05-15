8a875fd9a21684b84b0224b39a660618b186b5d7
Dansot4891 <cnctlim94@gmail.com>
2025-05-15 14:53:17 +0900

    chore: sdk 버전 세팅

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ require(detStorePassword != null) { "storePassword not found in key.properties f
 
 android {
     namespace = "com.kr.jogakjogak"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 33
     // NDK 버전을 명시적으로 설정
     ndkVersion = "27.0.12077973"
 
@@ -55,7 +55,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
