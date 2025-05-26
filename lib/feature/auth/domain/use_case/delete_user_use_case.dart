06c4e15203d87b1ef347152dbec9b33ab8309c40
Dansot4891 <cnctlim94@gmail.com>
2025-05-27 00:43:48 +0900

    [feat] 회원탈퇴에서 이메일/비밀번호 잘못 입력시의 상세 에러 처리 구현

--- lib/feature/auth/domain/use_case/delete_user_use_case.dart
+++ lib/feature/auth/domain/use_case/delete_user_use_case.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:jogak_jogak/core/constants/app_error_text.dart';
 import 'package:jogak_jogak/core/module/error_handling/result.dart';
 import 'package:jogak_jogak/core/module/exception/custom_exception.dart';
 import 'package:jogak_jogak/feature/auth/domain/repository/auth_repository.dart';
@@ -21,6 +23,11 @@ class DeleteUserUseCase {
       await _authRepository.deleteUser(email: email, password: password);
       await _userRepository.withdrawal();
       return const Result.success(null);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == AppErrorText.invalidCredential) {
+        return const Result.error(FaileSignInWithInvalidCredentialException());
+      }
+      return const Result.error(UnexpectedException());
     } catch (e) {
       return const Result.error(UnexpectedException());
     }
